c93af5cb81310a779f622d57a19f4f0d35c617fd
ksotis <ks.srikittichai@gmail.com>
2021-04-11 12:20:00 +0700

    update required env

--- nodejs/src/test/ENV.spec.ts
+++ nodejs/src/test/ENV.spec.ts
@@ -1,7 +1,7 @@
 describe('ENV', () => {
   it('should contain required env', () => {
-    expect(process.env.STORAGE_ACCESS_KEY).toBeDefined()
-    expect(process.env.STORAGE_SECRET_KEY).toBeDefined()
+    expect(process.env.AWS_ACCESS_KEY_ID).toBeDefined()
+    expect(process.env.AWS_SECRET_ACCESS_KEY).toBeDefined()
     expect(process.env.STORAGE_REGION).toBeDefined()
     expect(process.env.STORAGE_BUCKET).toBeDefined()
   })
